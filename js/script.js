f09f990eaa9b2b604a40c6ed83874e375197787b
Michel Maciel <michel.asm@hotmail.com>
2023-03-30 20:47:43 -0300

    deixarScrollDosLinksInternosSuaves transformado em Classe

--- js/script.js
+++ js/script.js
@@ -2,19 +2,21 @@ import aparecerRespostasFaqAcorddion from './modules/aparecer-respostas-faq-acor
 import aparecerSection from './modules/aparecer-section.js';
 import criarSemaforoDeFuncionamento from './modules/criar-semaforo-de-funcionamento.js';
 import criarTooltipDoMapa from './modules/criar-tooltip-do-mapa.js';
-import deixarScrollDosLinksInternosSuave from './modules/deixar-scroll-dos-links-internos-suave.js';
 import iniciarMenuMobile from './modules/iniciar-menu-mobile.js';
 import iniciarModal from './modules/iniciar-modal.js';
 import iniciarSobreSubmenu from './modules/iniciar-sobre-submenu.js';
 import juntarFotosComSuasDescricoes from './modules/juntar-fotos-com-suas-descricoes.js';
 import requisitarFetchNumerosDosAnimais from './modules/requisitar-fetch-numeros-dos-animais.js';
 import requisitarFetchPrecoDoBitcoin from './modules/requisitar-fetch-preco-do-bitcoin.js';
+import ScrollDosLinksInternosSuave from './modules/scroll-dos-links-internos-suave.js';
+
+const initScrollSuave = new ScrollDosLinksInternosSuave('[data-links-internos] a[href^="#"]');
+initScrollSuave.init();
 
 aparecerRespostasFaqAcorddion();
 aparecerSection();
 criarSemaforoDeFuncionamento();
 criarTooltipDoMapa();
-deixarScrollDosLinksInternosSuave();
 iniciarMenuMobile();
 iniciarModal();
 iniciarSobreSubmenu();
