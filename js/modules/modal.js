b2fe7a16087fa82b6ce2bf26ab7d886d2900efb0
Michel Maciel <michel.asm@hotmail.com>
2023-04-01 23:02:43 -0300

    iniciarModal transformado em Classe

---
+++ js/modules/modal.js
@@ -0,0 +1,37 @@
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, sectionGeralModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.sectionGeralModal = document.querySelector(sectionGeralModal);
+
+    this.abrirOUfecharModal = this.abrirOUfecharModal.bind(this);
+    this.cliqueForaDoModal = this.cliqueForaDoModal.bind(this);
+  }
+
+  adicionarEventListener() {
+    this.botaoAbrir.addEventListener('click', this.abrirOUfecharModal);
+    this.botaoFechar.addEventListener('click', this.abrirOUfecharModal);
+    this.sectionGeralModal.addEventListener('click', this.cliqueForaDoModal);
+  }
+
+  previnirPadrao(event) {
+    event.preventDefault();
+    this.abrirOUfecharModal(event);
+  }
+
+  abrirOUfecharModal() {
+    this.sectionGeralModal.classList.toggle('ativo');
+  }
+
+  cliqueForaDoModal(event) {
+    if (event.target === this.sectionGeralModal) this.abrirOUfecharModal(event);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.sectionGeralModal) {
+      this.adicionarEventListener();
+    }
+    return this;
+  }
+
+}
